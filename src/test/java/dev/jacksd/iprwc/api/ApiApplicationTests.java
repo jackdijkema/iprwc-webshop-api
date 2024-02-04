370ee7061f4c542ebd1e14ca05fb990cdc5c2e88
Jack <jackdijkema@hotmail.com>
2024-02-04 15:04:16 +0100

    INIT

---
+++ src/test/java/dev/jacksd/iprwc/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jacksd.iprwc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
