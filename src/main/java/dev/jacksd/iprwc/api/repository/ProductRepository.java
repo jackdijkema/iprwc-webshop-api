bec7d40651dd6502692d82794a8855a0b5cccfbe
Jack <jackdijkema@hotmail.com>
2024-02-04 20:50:59 +0100

    FIX: changed crudrepo to jparepo

--- src/main/java/dev/jacksd/iprwc/api/repository/ProductRepository.java
+++ src/main/java/dev/jacksd/iprwc/api/repository/ProductRepository.java
@@ -1,8 +1,14 @@
-package dev.jacksd.iprwc.api.repositories;
+package dev.jacksd.iprwc.api.repository;
 
 import dev.jacksd.iprwc.api.model.Product;
+import dev.jacksd.iprwc.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository extends CrudRepository<Product, UUID> { }
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> { }
